e6d213ad606d9c3836142bcdfbc0b684af15f14b
ucladevxtest <ucladevxtest@gmail.com>
2024-11-11 12:24:36 -0800

    Update App.js

--- react/src/App.js
+++ react/src/App.js
@@ -7,7 +7,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        Full stack template!
+        Full stack template! changed
       </header>
     </div>
   );
