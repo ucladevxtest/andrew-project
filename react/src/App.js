af3f0ef93de99c6a890a41ef5736aa4af5436864
Andrew Cruz <75159360+cruizeship@users.noreply.github.com>
2024-11-19 20:00:06 -0800

    getting ready for deployment

--- react/src/App.js
+++ react/src/App.js
@@ -7,7 +7,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        I am making a change on my new branch "andrew-changes"
+        Andrew is awesome
       </header>
     </div>
   );
